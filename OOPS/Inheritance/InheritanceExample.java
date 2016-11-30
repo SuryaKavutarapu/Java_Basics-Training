e27130bc1a39395f9c51a83d80b13a420e4a8333
SuryaKavutarapu <suryakavutarapu@hotmail.com>
2016-11-30 10:36:33 +0530

    Inheritance topic explained 30 nov 2016

--- OOPS/Inheritance/InheritanceExample.java
+++ OOPS/Inheritance/InheritanceExample.java
@@ -1,6 +1,6 @@
 class Parts{
     //created a parent class parts 
-
+   int a;
    //parent class methods car and bike 
    void car(){
        System.out.println("you have selected car");
@@ -11,12 +11,11 @@ class Parts{
 }
 //child class selection whit extends parent class Parts
 class Selection extends Parts{
-      Selection s = new Selection();
-    //object creation and calling methods using reference variable
+ 
     void select(){
       // s now can access parts class methods because selection class extends parts class
-      s.car();
-      s.bike();
+      car();
+      bike();
     }
 }
 //child class Myvalue whit extends parent class Selection note that selection is a child to the parts class so we can  access the parts method via Myvalue class reference variable
@@ -24,8 +23,11 @@ class MyValue extends Selection{
     public static void main(String []args){
  //object creation and calling methods using reference variable
         MyValue MY = new MyValue();
-        // MY now can access parts class methods because selection class extends parts class as well as MY can access selection methods because Myvalue class Extends Parts class
+        // MY now can access parts class methods because selection class extends parts class as well as MY can access selection methods because Myvalue class Extends selection class
         MY.select();
         MY.car();
+        MY.a = 10;
+
+        System.out.println(MY.a);
     }
 }
