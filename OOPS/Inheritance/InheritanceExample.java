9e0c088549ce378b49d3420e34b07f5c76bb7836
SuryaKavutarapu <suryakavutarapu@hotmail.com>
2016-11-30 10:26:34 +0530

    Inheritance topic explained 30 nov 2016

--- OOPS/Inheritance/InheritanceExample.java
+++ OOPS/Inheritance/InheritanceExample.java
@@ -1,4 +1,7 @@
 class Parts{
+    //created a parent class parts 
+
+   //parent class methods car and bike 
    void car(){
        System.out.println("you have selected car");
    }
@@ -6,18 +9,22 @@ class Parts{
         System.out.println("you have selected bike");
    }
 }
-
+//child class selection whit extends parent class Parts
 class Selection extends Parts{
-    void select(){
       Selection s = new Selection();
+    //object creation and calling methods using reference variable
+    void select(){
+      // s now can access parts class methods because selection class extends parts class
       s.car();
       s.bike();
     }
 }
-
+//child class Myvalue whit extends parent class Selection note that selection is a child to the parts class so we can  access the parts method via Myvalue class reference variable
 class MyValue extends Selection{
     public static void main(String []args){
+ //object creation and calling methods using reference variable
         MyValue MY = new MyValue();
+        // MY now can access parts class methods because selection class extends parts class as well as MY can access selection methods because Myvalue class Extends Parts class
         MY.select();
         MY.car();
     }
---
+++ OOPS/Inheritance/IntroToINheritance.java
@@ -0,0 +1,38 @@
+/* 
+   relation b/w classes
+
+   IS - A (relationship) => inheritance
+   
+   Inheritance
+   ===========
+   1.new class is going constructed depends on  existance class
+      ex: 
+        if we created A class before and we want to write another class B the features can be obtain from Class A
+
+   2.Association with in the class is also named IS-A class type
+   3.parent to chaild relation
+
+
+     |  A class  |  ====>  |  B class |    ===> | C class |
+
+   A is called base class,Super class,Master class,base Class
+   B  derived class, child class,sub class
+
+   Implementation
+   ==============
+
+   class B extends  class A  
+     
+   Example:
+
+   Go to InheritanceExample.java 
+
+   There i Explained about Inheritance in detailed 
+*/
+
+
+
+
+
+
+
