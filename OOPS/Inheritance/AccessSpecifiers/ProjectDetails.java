9716eb661965c2b353920273e9838cb81a688ad5
SuryaKavutarapu <suryakavutarapu@hotmail.com>
2016-11-30 12:50:01 +0530

    Inheritance topic explained 30 nov 2016

--- OOPS/Inheritance/AccessSpecifiers/ProjectDetails.java
+++ OOPS/Inheritance/AccessSpecifiers/ProjectDetails.java
@@ -2,40 +2,50 @@ class Company{
   protected String Cname;
   protected String Ctype;
   protected String Clocation;
-  
+  //initializing protected strings
+
+  //method readCompanyDetails
   public void readCompanyDetails(String Cname,String Ctype, String Clocation){
      this.Cname = Cname;
      this.Ctype = Ctype;
      this.Clocation =Clocation;
+     //assigning values to corresponding variables
   }
-}
+}/parent class
 
 class Employee extends Company{
   protected String Ename;
   protected String Eid;
   protected String Edesegnation;
-  
+  //initializing protected strings
   public void readEmployeeDetails(String Ename,String Eid, String Edesegnation){
      this.Eid = Eid;
      this.Ename = Ename;
      this.Edesegnation =Edesegnation;
+       //assigning values to corresponding variables
      readCompanyDetails("Orchesys","Service Based","Banglore");
+     //calling parent class method readCompanyDetails and passing argumets
   } 
-}
+}//parent class  to project and child class to Company
 
 
 class Project extends Employee{
    private String Pname;
    private String PId;
    private String Pduration;
-   {
+   //initializing protected strings only we can access in that class only
+   { 
+       //assigning private String values in Instance initialization block because of private keyword
        Pname = "Railway";
        PId ="P-1617";
        Pduration="3months";
        readprojectDetails();
+       //calling private method in its own class
    }
+
    private void readprojectDetails(){
-        readEmployeeDetails("SuryaKavutarapu","ORITS-332","Java Developer");        
+        readEmployeeDetails("SuryaKavutarapu","ORITS-332","Java Developer");  
+        //calling parent class method readEmployeeDetails and passing argumets      
    }
 
    public void display(){
@@ -53,7 +63,7 @@ class Project extends Employee{
 class ProjectDetails{
     public static void main(String []args){
        Project p = new Project();
-       p.display();
+       p.display();// calling Display method
     }
 }
 
