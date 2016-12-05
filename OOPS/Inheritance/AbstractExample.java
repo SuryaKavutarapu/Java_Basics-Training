30a489102e48aa24d496893c801f801443a09bc3
SuryaKavutarapu <suryakavutarapu@hotmail.com>
2016-12-05 16:33:41 +0530

    abstract methods explained 05 dec 2016

--- OOPS/Inheritance/AbstractExample.java
+++ OOPS/Inheritance/AbstractExample.java
@@ -2,20 +2,20 @@ class AbstractExample{
     public static void main(String []args){
         GometryFigure g;//parent class reference
         g = new Triangle();//child class object
-        g.area(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
-        System.out.println(g.length);
-        System.out.println(g.height);
+        g.area(Integer.parseInt(args[0]),Integer.parseInt(args[1]));//calling area method in triangle class
+        System.out.println(g.length);//prints args[0] value
+        System.out.println(g.height);//prints args[1] value
         g = new Rectangle();//child class object
-        g.area(Integer.parseInt(args[2]),Integer.parseInt(args[3]));
-        System.out.println(g.length);
-        System.out.println(g.height);
+        g.area(Integer.parseInt(args[2]),Integer.parseInt(args[3]));//calling area method in rectangle class
+        System.out.println(g.length);//prints args[2] value
+        System.out.println(g.height);//prints args[3] value
     }
 }
 
 
 abstract class GometryFigure{
    int length,height;
-
+   double area;
    abstract void area(int length,int height);
    //declared an abstract method named area
 }
@@ -25,7 +25,7 @@ class Triangle extends GometryFigure{
   public void area(int length,int height){
       this.length = length;
       this.height = height;
-      double area =  0.5 * (length * height);
+      this.area =  0.5 * (length * height);
       System.out.println("area of triangle "+area);
   }   
 }
@@ -35,7 +35,7 @@ class Rectangle extends GometryFigure{
   public void area(int length,int height){
       this.length = length;
       this.height = height;
-      int area =  (length * height);
+      this.area =  (length * height);
       System.out.println("area of rectangle "+area);
   }   
 }
