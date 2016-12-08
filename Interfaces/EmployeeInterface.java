b39a6486330dfe94980c2a801bb4f330b2eac1f4
SuryaKavutarapu <suryakavutarapu@hotmail.com>
2016-12-08 17:12:03 +0530

    Employee Interface Example 08 dec 2016

--- Interfaces/EmployeeInterface.java
+++ Interfaces/EmployeeInterface.java
@@ -1,1 +1,54 @@
-interface Em
+interface EmployeeInterface{
+    public abstract void input();
+    public abstract void display();
+}
+
+class Student implements EmployeeInterface{
+    public  int Sid;
+    public  String Sname,Sbranch;
+    
+    public void setStudent(int Sid,String Sname,String Sbranch){
+        this.Sid = Sid;
+        this.Sname = Sname;
+        this.Sbranch = Sbranch;
+    }
+    public void input(){
+        setStudent(322,"SuryaKavutarapu","EEE");
+    }
+
+    public void display(){
+        System.out.println("Student Id: "+Sid+"\nStudent Name: "+Sname+"\nStudent branch: "+Sbranch);
+    }
+}
+
+class Employee implements EmployeeInterface {
+    public  int Employeeid;
+    public  String Employeename,Employeebranch,EmployeeCompany;
+    
+    public void setEmployee(int Employeeid,String Employeename,String Employeebranch,String EmployeeCompany){
+        this.Employeeid = Employeeid;
+        this.Employeename = Employeename;
+        this.Employeebranch = Employeebranch;
+        this.EmployeeCompany = EmployeeCompany;
+    }
+    public void input(){
+        setEmployee(322,"SuryaKavutarapu","Banglore","Orchesys IT");
+    }
+
+    public void display(){
+        System.out.println("Employee Id: "+Employeeid+"\nEmployee Name: "+Employeename+"\nEmployee Company"+EmployeeCompany+"\nEmployee branch: "+Employeebranch);
+    }
+}
+
+
+
+class EmployeeInterfaceExample{
+  public static void main(String []args){
+      EmployeeInterface student =  new Student();
+      student.input();
+      student.display();
+      EmployeeInterface employee =  new Employee();
+      employee.input();
+      employee.display();
+  }/*method main programm execution starts from here*/
+}/*class EmployeeInterfaceExample*/
