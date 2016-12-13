8c8020e4a12e09fd44b740e7a2feb1f163044c20
SuryaKavutarapu <suryakavutarapu@hotmail.com>
2016-12-13 10:21:53 +0530

    packages moved to another repository 12 dec 2016

--- Packages/A.java
+++
@@ -1,9 +0,0 @@
-package abc;
-//package name should be small caps
-
-public class A{
-  public void show(){
-    System.out.println("This is a A class");
-  }/*method show*/
-}/*class A*/
-
--- Packages/B.java
+++
@@ -1,8 +0,0 @@
-package abc;
-//package name should be small caps
-
-public class B{
-  public void show(){
-    System.out.println("This is a B class");
-  }/*method show*/
-}/*class B*/
